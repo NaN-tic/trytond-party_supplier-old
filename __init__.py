3b8ba896aabd55e1c5a0e8a64446a33f8eb3d018
Raimon Esteve <raimon@nan-tic.com>
2017-11-27 23:49:03 +0100

    Add supplier party domain in product supplier task-032809

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .party import *
-from .purchase import *
+from . import party
+from . import purchase
+from . import product
 
 
 def register():
     Pool.register(
-        Party,
-        Purchase,
+        party.Party,
+        purchase.Purchase,
+        product.ProductSupplier,
         module='party_supplier', type_='model')
